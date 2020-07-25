4dbb5750ab27cf1f2842d33bbd602b7b21542756
Frederick-88 <frederickfd88@gmail.com>
2020-07-25 11:19:25 +0700

    update

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ Vue.component("product", {
       details: [
         "80% Cotton",
         "20% Polyster",
-        "Gendera Neutral (Both Men & Women)",
+        "Gender Neutral (Both Men & Women)",
       ],
       variants: [
         {
